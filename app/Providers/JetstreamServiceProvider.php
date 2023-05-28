a803a01367f2ba95057a6640e2540cc49f781cbd
engsahaly <engsahaly@gmail.com>
2023-05-28 23:15:17 +0200

    jetstream basic features

--- app/Providers/JetstreamServiceProvider.php
+++ app/Providers/JetstreamServiceProvider.php
@@ -39,6 +39,8 @@ class JetstreamServiceProvider extends ServiceProvider
         Jetstream::deleteUsersUsing(DeleteUser::class);
 
         $this->configureRoutes();
+
+        // Jetstream::deleteUsersUsing();
     }
 
     /**
--- resources/markdown/terms.md
+++ resources/markdown/terms.md
@@ -1,3 +1,5 @@
 # Terms of Service
 
 Edit this file to define the terms of service for your application.
+
+_here is my terms text_
